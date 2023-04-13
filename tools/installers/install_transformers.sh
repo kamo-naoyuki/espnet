9a81998133c11d713bc6b2a1ad0e771eb3ab634b
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2023-04-14 01:24:11 +0900

    modified:   installers/install_transformers.sh

--- tools/installers/install_transformers.sh
+++ tools/installers/install_transformers.sh
@@ -6,4 +6,24 @@ if [ $# != 0 ]; then
     exit 1;
 fi
 
-python3 -m pip install "transformers>=4.9.1"
+if ! python -c "import packaging.version" &> /dev/null; then
+    python3 -m pip install packaging
+fi
+torch_version=$(python3 -c "import torch; print(torch.__version__)")
+
+pt_plus(){
+    python3 <<EOF
+import sys
+from packaging.version import parse as L
+if L('$torch_version') >= L('$1'):
+    print("true")
+else:
+    print("false")
+EOF
+}
+
+if "$(pt_plus 1.9)"; then
+    python3 -m pip install "transformers>=4.9.1"
+else
+    echo "transformers requires pytorch>=1.9"
+fi
